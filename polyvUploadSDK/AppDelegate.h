5d4ddfd690f7d96dea2519fbf06b2a6d40ff0ee9
Zeaa <fft1026@163.com>
2016-06-21 11:31:19 +0800

    initialize the project

---
+++ polyvUploadSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  polyvUploadSDK
+//
+//  Created by ftao on 16/6/21.
+//  Copyright © 2016年 easefun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ polyvUploadSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  polyvUploadSDK
+//
+//  Created by ftao on 16/6/21.
+//  Copyright © 2016年 easefun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
